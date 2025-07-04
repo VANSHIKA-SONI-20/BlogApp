14e651cf3246277f5de4a5cf3080c7069fee7f1f
VANSHIKA-SONI-20 <sonivanshika630@gmail.com>
2025-07-04 12:06:21 +0530

    commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo(width = '100px') {
+  return (
+    <img src="https://tse3.mm.bing.net/th?id=OIP.4dcJ_AHTJ81dikKbJ_xBtgHaGw&rs=1&pid=ImgDetMain" className='w-10' alt="" />
+  )
+}
+
+export default Logo
---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import Container from "./Container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE"
+import Signup from "./Signup"
+import Login from "./Login"
+import PostForm from "./PostForm/PostForm"
+import Select from "./Select"
+import PostCard from "./PostCard"
+import Input from "./input"
+import AuthLayout from "./AuthLayout"
+import Button from "./Footer/Button"
+
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    Select,
+    PostCard,
+    Input,
+    AuthLayout,
+    Button
+}
